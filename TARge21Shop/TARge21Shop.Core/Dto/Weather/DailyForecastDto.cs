cfbb2708efed63452467a2a3c8115cd6b48431dc
Opilane TTHK <andreluige@hot.ee>
2023-03-02 11:38:25 +0200

    Working on Weather Forecasts

--- TARge21Shop/TARge21Shop.Core/Dto/Weather/DailyForecastDto.cs
+++ TARge21Shop/TARge21Shop.Core/Dto/Weather/DailyForecastDto.cs
@@ -1,36 +1,70 @@
-﻿namespace TARge21Shop.Core.Dto.Weather
+﻿using System.Text.Json.Serialization;
+
+namespace TARge21Shop.Core.Dto.Weather
 {
     public class DailyForecastDto
     {
+        [JsonPropertyName("Date")]
         public DateTime Date { get; set; }
+
+        [JsonPropertyName("EpochDate")]
         public int EpochDate { get; set; }
+
+        [JsonPropertyName("Temperature")]
         public Temperatures Temperature { get; set; }
+
+        [JsonPropertyName("Day")]
         public DayNightCycle Day { get; set; }
+
+        [JsonPropertyName("Night")]
         public DayNightCycle Night { get; set; }
+
+        [JsonPropertyName("Sources")]
         public List<string> Sources { get; set; }
+
+        [JsonPropertyName("MobileLink")]
         public string MobileLink { get; set; }
+
+        [JsonPropertyName("Link")]
         public string Link { get; set; }
     }
 
     public class DayNightCycle
     {
+        [JsonPropertyName("Icon")]
         public int Icon { get; set; }
+
+        [JsonPropertyName("IconPhrase")]
         public string IconPhrase { get; set; }
+
+        [JsonPropertyName("HasPrecipitation")]
         public bool HasPrecipitation { get; set; }
+
+        [JsonPropertyName("PrecipitationType")]
         public string PrecipitationType { get; set; }
+
+        [JsonPropertyName("PrecipitationIntensity")]
         public string PrecipitationIntensity { get; set; }
     }
 
     public class Temperature
     {
+        [JsonPropertyName("Value")]
         public double Value { get; set; }
+
+        [JsonPropertyName("Unit")]
         public string Unit { get; set; }
+
+        [JsonPropertyName("UnitType")]
         public int UnitType { get; set; }
     }
 
     public class Temperatures
     {
+        [JsonPropertyName("Minimum")]
         public Temperature Minimum { get; set; }
+
+        [JsonPropertyName("Maximum")]
         public Temperature Maximum { get; set; }
     }
 }
---
+++ TARge21Shop/TARge21Shop/Controllers/WeatherForecastsController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TARge21Shop.Core.Dto.Weather;
+using TARge21Shop.Core.ServiceInterface;
+using TARge21Shop.Data;
+using TARge21Shop.Models.Weather;
+
+namespace TARge21Shop.Controllers
+{
+    public class WeatherForecastsController : Controller
+    {
+        private readonly TARge21ShopContext _context;
+        private readonly IWeatherForecastsServices _weatherForecastsServices;
+
+        public WeatherForecastsController
+            (
+                TARge21ShopContext context,
+                IWeatherForecastsServices weatherForecastsServices
+            )
+        {
+            _context = context;
+            _weatherForecastsServices = weatherForecastsServices;
+        }
+
+        public IActionResult Index()
+        {
+            WeatherViewModel vm = new WeatherViewModel();
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult ShowWeather(WeatherViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("City", "WeatherForecasts");
+            }
+
+            return View(vm);
+        }
+
+        [HttpGet]
+        public IActionResult City()
+        {
+            WeatherResultDto dto = new WeatherResultDto();
+            WeatherViewModel vm = new WeatherViewModel();
+
+            _weatherForecastsServices.WeatherDetail(dto);
+
+            vm.Date = dto.Date;
+            vm.EpochDate = dto.EpochDate;
+
+            vm.Temperature.Minimum.Value = dto.MinimumValue;
+            vm.Temperature.Minimum.Unit = dto.MinimumUnit;
+            vm.Temperature.Minimum.UnitType = dto.MinimumUnitType;
+
+            vm.Temperature.Maximum.Value = dto.MaximumValue;
+            vm.Temperature.Maximum.Unit = dto.MaximumUnit;
+            vm.Temperature.Maximum.UnitType = dto.MaximumUnitType;
+
+            vm.Day.Icon = dto.DayIcon;
+            vm.Day.IconPhrase = dto.DayIconPhrase;
+            vm.Day.HasPrecipitation = dto.DayHasPrecipitation;
+            vm.Day.PrecipitationType = dto.DayPrecipitationType;
+            vm.Day.PrecipitationIntensity = dto.DayPrecipitationIntensity;
+
+            vm.Night.Icon = dto.NightIcon;
+            vm.Night.IconPhrase = dto.NightIconPhrase;
+            vm.Night.HasPrecipitation = dto.NightHasPrecipitation;
+            vm.Night.PrecipitationType = dto.NightPrecipitationType;
+            vm.Night.PrecipitationIntensity = dto.NightPrecipitationIntensity;
+
+            vm.Sources = dto.Sources;
+            vm.MobileLink = dto.MobileLink;
+            vm.Link = dto.Link;
+
+            return View(vm);
+        }
+    }
+}
