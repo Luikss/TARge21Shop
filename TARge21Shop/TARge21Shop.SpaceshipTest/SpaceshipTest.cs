ec5935082702760e8102422ceacd2382d4cbf3e7
Opilane TTHK <andreluige@hot.ee>
2023-03-09 19:10:07 +0200

    Added test project and base

---
+++ TARge21Shop/TARge21Shop.SpaceshipTest/SpaceshipTest.cs
@@ -0,0 +1,32 @@
+﻿using TARge21Shop.Core.Dto;
+using TARge21Shop.Core.ServiceInterface;
+using Xunit;
+
+namespace TARge21Shop.SpaceshipTest
+{
+    public class SpaceshipTest : TestBase
+    {
+        [Fact]
+        public async Task ShouldNot_AddEmptySpaceship_WhenReturnResult()
+        {
+            SpaceshipDto spaceship = new SpaceshipDto();
+            spaceship.Id = Guid.NewGuid();
+            spaceship.Name = "asd";
+            spaceship.Type = "asd";
+            spaceship.Crew = 123;
+            spaceship.Passengers = 123;
+            spaceship.CargoWeight = 123;
+            spaceship.FullTripsCount = 123;
+            spaceship.MaintenanceCount = 123;
+            spaceship.LastMaintenance = DateTime.Now;
+            spaceship.EnginePower = 123;
+            spaceship.MaidenLaunch = DateTime.Now;
+            spaceship.BuiltDate = DateTime.Now;
+            spaceship.CreatedAt = DateTime.Now;
+            spaceship.ModifiedAt = DateTime.Now;
+
+            var result = await Svc<ISpaceshipsServices>().Create(spaceship);
+            Assert.NotNull(result);
+        }
+    }
+}
