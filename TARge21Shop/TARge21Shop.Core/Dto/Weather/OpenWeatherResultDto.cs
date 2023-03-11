b539f3e10f63214401d265f7648906cd9e4ba3ad
Andre Luige <Andre.Luige@nortal.com>
2023-03-11 21:12:01 +0200

    OpenWeather result DTO

---
+++ TARge21Shop/TARge21Shop.Core/Dto/Weather/OpenWeatherResultDto.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TARge21Shop.Core.Dto.Weather
+{
+    public class OpenWeatherResultDto
+    {
+        public int Timezone { get; set; }
+        public string Name { get; set; }
+        public double Lon { get; set; }
+        public double Lat { get; set; }
+        public double Temp { get; set; }
+        public double Feels_like { get; set; }
+        public int Pressure { get; set; }
+        public int Humidity { get; set; }
+        public string Main { get; set; }
+        public string Description { get; set; }
+        public double Speed { get; set; }
+    }
+}
