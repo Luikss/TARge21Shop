c6d100f357f9d992feb28de3e787fc741ff24f5c
Opilane TTHK <andreluige@hot.ee>
2023-03-16 17:05:25 +0200

    IHostingEnvironment mock and refactored test class

--- TARge21Shop/TARge21Shop.SpaceshipTest/SpaceshipTest.cs
+++ TARge21Shop/TARge21Shop.SpaceshipTest/SpaceshipTest.cs
@@ -1,4 +1,5 @@
-﻿using TARge21Shop.Core.Dto;
+﻿using TARge21Shop.Core.Domain;
+using TARge21Shop.Core.Dto;
 using TARge21Shop.Core.ServiceInterface;
 using Xunit;
 
@@ -9,8 +10,32 @@ namespace TARge21Shop.SpaceshipTest
         [Fact]
         public async Task ShouldNot_AddEmptySpaceship_WhenReturnResult()
         {
+            SpaceshipDto spaceship = createSpaceshipDto();
+            var result = await Svc<ISpaceshipsServices>().Create(spaceship);
+            assertSpaceships(spaceship, result);
+        }
+
+        internal void assertSpaceships(SpaceshipDto expected, Spaceship actual)
+        {
+            Assert.NotNull(actual.Id);
+            Assert.NotNull(actual.CreatedAt);
+            Assert.NotNull(actual.ModifiedAt);
+            Assert.Equal(expected.Name, actual.Name);
+            Assert.Equal(expected.Type, actual.Type);
+            Assert.Equal(expected.Crew, actual.Crew);
+            Assert.Equal(expected.Passengers, actual.Passengers);
+            Assert.Equal(expected.CargoWeight, actual.CargoWeight);
+            Assert.Equal(expected.FullTripsCount, actual.FullTripsCount);
+            Assert.Equal(expected.MaintenanceCount, actual.MaintenanceCount);
+            Assert.Equal(expected.LastMaintenance, actual.LastMaintenance);
+            Assert.Equal(expected.EnginePower, actual.EnginePower);
+            Assert.Equal(expected.MaidenLaunch, actual.MaidenLaunch);
+            Assert.Equal(expected.BuiltDate, actual.BuiltDate);
+        }
+
+        public static SpaceshipDto createSpaceshipDto()
+        {
             SpaceshipDto spaceship = new SpaceshipDto();
-            spaceship.Id = Guid.NewGuid();
             spaceship.Name = "asd";
             spaceship.Type = "asd";
             spaceship.Crew = 123;
@@ -22,11 +47,8 @@ namespace TARge21Shop.SpaceshipTest
             spaceship.EnginePower = 123;
             spaceship.MaidenLaunch = DateTime.Now;
             spaceship.BuiltDate = DateTime.Now;
-            spaceship.CreatedAt = DateTime.Now;
-            spaceship.ModifiedAt = DateTime.Now;
 
-            var result = await Svc<ISpaceshipsServices>().Create(spaceship);
-            Assert.NotNull(result);
+            return spaceship;
         }
     }
 }
